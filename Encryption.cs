294c97b03d6aebf31de92a175afc42b32b6dadef
Cuyler36 <Cuyler36@users.noreply.github.com>
2020-03-17 22:16:54 -0400

    Implement Encryption

--- Encryption.cs
+++ Encryption.cs
@@ -40,5 +40,46 @@ namespace HorizonCrypt
             transform.TransformBlock(encData, 0, encData.Length, decData, 0);
             return decData;
         }
+
+        private static uint[] PrependedData =
+        {
+            0x00000067, 0x0000006F, 0x00000002, 0x00000002, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+            0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+            0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+            0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+            0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+            0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+            0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+            0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000
+        };
+
+        private static (byte[], byte[], byte[]) GenerateHeaderFile()
+        {
+            var encryptData = new uint[0x80];
+
+            // Generate 128 Random uints which will be used for params
+            var random = new SeadRandom((uint)DateTime.Now.Ticks);
+            for (var i = 0; i < 128; i++)
+                encryptData[i] = random.GetU32();
+
+            var headerData = new byte[0x300];
+            Buffer.BlockCopy(PrependedData, 0, headerData, 0, 0x100);
+            Buffer.BlockCopy(encryptData, 0, headerData, 0x100, 0x200);
+            return (headerData, GetParam(encryptData, 0), GetParam(encryptData, 2));
+        }
+
+        public static (byte[], byte[]) Encrypt(in byte[] data)
+        {
+            // Generate header file and get key and counter
+            var (headerData, key, ctr) = GenerateHeaderFile();
+
+            // Encrypt file
+            using var aesCtr = new Aes128CounterMode(ctr);
+            var transform = aesCtr.CreateEncryptor(key, ctr);
+            var encData = new byte[data.Length];
+            transform.TransformBlock(data, 0, data.Length, encData, 0);
+
+            return (encData, headerData);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,17 +7,34 @@ namespace HorizonCrypt
     {
         static void Main(string[] args)
         {
-            if (args?.Length < 1)
+            if (args?.Length < 2 || (args[0] != "-c" && args[0] != "-d") || !File.Exists(args[1]))
             {
-                Console.WriteLine("Enter the path to the encrypted save file:");
-                args = new string[] { Console.ReadLine().Replace("\"", "") };
+                PrintUsage();
+                return;
             }
 
-            var directory = Path.GetDirectoryName(args[0]);
-            var header = Path.Combine(directory, Path.GetFileNameWithoutExtension(args[0]) + "Header.dat");
-            var file = args[0];
-            var data = Encryption.Decrypt(File.ReadAllBytes(header), File.ReadAllBytes(file));
-            File.WriteAllBytes(Path.Combine(directory, Path.GetFileNameWithoutExtension(args[0]) + "_decrypted.dat"), data);
+            if (args[0] == "-d")
+            {
+                var directory = Path.GetDirectoryName(args[1]);
+                var header = Path.Combine(directory, Path.GetFileNameWithoutExtension(args[1]) + "Header.dat");
+                var data = Encryption.Decrypt(File.ReadAllBytes(header), File.ReadAllBytes(args[1]));
+                File.WriteAllBytes(Path.Combine(directory, Path.GetFileNameWithoutExtension(args[1]) + "_decrypted.dat"), data);
+            }
+            else
+            {
+                var directory = Path.GetDirectoryName(args[1]);
+                var (data, headerData) = Encryption.Encrypt(File.ReadAllBytes(args[1]));
+                
+                File.WriteAllBytes(Path.Combine(directory, Path.GetFileNameWithoutExtension(args[1]) + "_encrypted.dat"), data);
+                File.WriteAllBytes(Path.Combine(directory, Path.GetFileNameWithoutExtension(args[1]) + "_encryptedHeader.dat"), headerData);
+            }
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("HorizonCrypt by Cuyler");
+            Console.WriteLine("Usage:");
+            Console.WriteLine("\tHorizonCrypt [-c|-d] <input>");
         }
     }
 }
