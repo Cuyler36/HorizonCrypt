6a9d25f643bc9fdea3cd99a406173e562d560427
Cuyler36 <Cuyler36@users.noreply.github.com>
2020-03-18 03:50:29 -0400

    Add Batch FIle Mode
    -b switch before the mode

--- HorizonCrypt.ConsoleApp/Program.cs
+++ HorizonCrypt.ConsoleApp/Program.cs
@@ -5,13 +5,21 @@ namespace HorizonCrypt.ConsoleApp
 {
     internal static class Program
     {
-        private const string Encrypt = "-c";
-        private const string Decrypt = "-d";
+        private const string Encrypt   = "-c";
+        private const string Decrypt   = "-d";
+        private const string BatchMode = "-b";
+
+        private enum Mode
+        { 
+            Encrypt, Decrypt
+        }
 
         private static void Main(string[] args)
         {
             if (!TryExecute(args))
                 PrintUsage();
+            else
+                Console.WriteLine("Done!");
             Console.Read();
         }
 
@@ -20,41 +28,132 @@ namespace HorizonCrypt.ConsoleApp
             if (args.Length < 2)
                 return false;
 
-            var type = args[0];
-            var file = args[1];
-            var directory = Path.GetDirectoryName(file);
-            if (directory == null || !File.Exists(file))
+            var file = args[args.Length - 1];
+            if (!File.Exists(file) && !Directory.Exists(file))
                 return false;
 
-            var filename = Path.GetFileNameWithoutExtension(file);
-            switch (type)
+            file = Path.GetFullPath(file);
+            var directory = Path.GetDirectoryName(file);
+            if (directory == null)
+                return false;
+
+            var argumentsValid = false;
+            var batchMode = false;
+            var mode = Mode.Decrypt;
+            for (var i = 0; i < args.Length - 1; i++)
             {
-                case Decrypt:
+                var type = args[i];
+                switch (type)
                 {
-                    var headerPath = Path.Combine(directory, filename + "Header.dat");
-                    var headerData = File.ReadAllBytes(headerPath);
-                    var encData = File.ReadAllBytes(file);
+                    case Decrypt:
+                        {
+                            if (argumentsValid)
+                                return false;
+                            mode = Mode.Decrypt;
+                            argumentsValid = true;
+                            break;
+                        }
 
-                    var decrypted = Encryption.Decrypt(headerData, encData);
-                    File.WriteAllBytes(Path.Combine(directory, filename + "_decrypted.dat"), decrypted);
-                    Console.WriteLine("Decrypted!");
+                    case Encrypt:
+                        {
+                            if (argumentsValid)
+                                return false;
+                            mode = Mode.Encrypt;
+                            argumentsValid = true;
+                            break;
+                        }
+
+                    case BatchMode:
+                        {
+                            batchMode = true;
+                            break;
+                        }
+
+                    default:
+                        return false;
+                }
+            }
+
+            if (!argumentsValid)
+                return false;
+
+            // Create out directory to preserve original files.
+            var filename = Path.GetFileName(file);
+            if (batchMode && string.IsNullOrWhiteSpace(filename))
+            {
+                file = directory;
+                filename = Path.GetFileName(file);
+                directory = Path.GetDirectoryName(directory);
+            }
+
+            var outDir = Path.Combine(directory, filename + (mode == Mode.Decrypt ? " Decrypted" : " Encrypted"));
+            Console.WriteLine("OutDir=" + outDir);
+
+            // In batch mode the file will be the root directory.
+            if (batchMode)
+            {
+                var rootDir = file;
+                if (Directory.Exists(rootDir))
+                {
+                    ProcessFolder(rootDir, outDir, mode);
                     return true;
                 }
 
-                case Encrypt:
+                return false;
+            }
+
+            // Process an individual file.
+            if (File.Exists(file))
+            {
+                ProcessFile(file, directory, outDir, mode);
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void ProcessFolder(in string directory, in string outDir, in Mode mode)
+        {
+            foreach (var file in Directory.GetFiles(directory, "*.dat"))
+                ProcessFile(file, directory, outDir, mode);
+
+            foreach (var subDir in Directory.GetDirectories(directory))
+                ProcessFolder(subDir, Path.Combine(outDir, Path.GetFileName(subDir)), mode);
+        }
+
+        private static void ProcessFile(in string file, in string directory, in string outDir, in Mode mode)
+        {
+            if (!Directory.Exists(outDir))
+                Directory.CreateDirectory(outDir);
+            var filename = Path.GetFileNameWithoutExtension(file);
+            if (!filename.Contains("Header"))
+            {
+                switch (mode)
                 {
-                    var decData = File.ReadAllBytes(file);
-                    var seed = (uint)DateTime.Now.Ticks;
-                    var (data, headerData) = Encryption.Encrypt(decData, seed);
+                    case Mode.Decrypt:
+                        {
+                            var headerPath = Path.Combine(directory, filename + "Header.dat");
+                            var headerData = File.ReadAllBytes(headerPath);
+                            var encData = File.ReadAllBytes(file);
 
-                    File.WriteAllBytes(Path.Combine(directory, filename + "_encrypted.dat"), data);
-                    File.WriteAllBytes(Path.Combine(directory, filename + "_encryptedHeader.dat"), headerData);
-                    Console.WriteLine("Encrypted!");
-                    return true;
+                            var decrypted = Encryption.Decrypt(headerData, encData);
+                            File.WriteAllBytes(Path.Combine(outDir, filename + ".dat"), decrypted);
+                            Console.WriteLine($"Decrypted: {file}");
+                            break;
+                        }
+
+                    case Mode.Encrypt:
+                        {
+                            var decData = File.ReadAllBytes(file);
+                            var seed = (uint)DateTime.Now.Ticks;
+                            var (data, headerData) = Encryption.Encrypt(decData, seed);
+
+                            File.WriteAllBytes(Path.Combine(outDir, filename + ".dat"), data);
+                            File.WriteAllBytes(Path.Combine(outDir, filename + "Header.dat"), headerData);
+                            Console.WriteLine($"Encrypted {file}");
+                            break;
+                        }
                 }
-
-                default:
-                    return false;
             }
         }
 
@@ -62,7 +161,7 @@ namespace HorizonCrypt.ConsoleApp
         {
             Console.WriteLine("HorizonCrypt by Cuyler");
             Console.WriteLine("Usage:");
-            Console.WriteLine("\tHorizonCrypt [-c|-d] <input>");
+            Console.WriteLine("\tHorizonCrypt [-b] [-c|-d] <input>");
         }
     }
 }
